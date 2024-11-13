deb40a45df397cdf8cdd6a2d4f006df841669910
Neo See <neococo123@yahoo.com>
2024-11-12 20:01:10 -0800

    dashboardstart

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,8 +1,22 @@
+import { Button } from '@chakra-ui/react'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    
+    <>
+        <h1 className='text-center'>Student Caseload</h1>
+
+        <div className='container'>
+            <div className="row">
+                <div className="col"></div>
+                <div className="col">
+                    <Button></Button>
+                </div>
+            </div>
+        </div>
+    </>
+
   )
 }
 
