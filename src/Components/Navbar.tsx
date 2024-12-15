6b3bf82b1c87f9903549fbef569c0a04aa128bd5
Raquelbateman <aliciaraquel1017@gmail.com>
2024-12-15 15:22:40 -0800

    updates to navbar using chakra working on responsiveness

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,43 +1,49 @@
 import { Navbar, Container } from "react-bootstrap";
 import Logo from "../assets/Logo.png";
 import { Link, NavLink } from "react-router-dom";
+import { Box, Flex } from "@chakra-ui/react";
 
 const NavbarComponent = () => {
     return (
-        <div className="navbar-wrapper">
-            <Navbar className="custom-navbar" data-bs-theme="dark">
-                <Container>
-                    <NavLink to={'/'}>
-                    <img
-                            src={Logo} 
-                            alt="Siete Logo" 
-                            className='sieteLogo'
-                        />
-                    </NavLink>
-                </Container>
-            </Navbar>
-            
-            <div className="bottom-bar">
-                <Container className="bottom-bar-content">
-                    <div className="left-links">
-                        <Link to={'/'} className='navTextLeft'>Home</Link>
-                        <Link to={'/Contact'} className='navTextLeft'>Contact Us</Link>
-                    </div>
-                    <div className="right-links">
-
-                        <Link to={'/Dashboard'} className='navTextRight'>Dashboard</Link>
-<<<<<<<<< Temporary merge branch 1
-                        <Link to={'/Login'} className='navTextRight'>Login</Link>
-=========
-                        <Link to={"Login"} className='navTextRight'>Login</Link>
->>>>>>>>> Temporary merge branch 2
-                        <Link to={'/CreateAccount'} className='navTextRight'>Create Account</Link>
-
-                    </div>
-                    </Container>
-            </div>
-        </div>
+        <Box className="navbar-wrapper">
+        <Navbar className="custom-navbar" data-bs-theme="dark">
+          <Container>
+            <NavLink to="/">
+              <img src={Logo} alt="Siete Logo" className="sieteLogo" />
+            </NavLink>
+          </Container>
+        </Navbar>
+  
+       
+        <Box className="bottom-bar">
+          <Container className="bottom-bar-content">
+            <Flex justify="space-between" align="center">
+           
+              <Flex>
+                <Link to="/" className="navTextLeft">
+                  Home
+                </Link>
+                <Link to="/Contact" className="navTextLeft">
+                  Contact Us
+                </Link>
+              </Flex>
+  
+              <Flex>
+                <Link to="/Dashboard" className="navTextRight">
+                  Dashboard
+                </Link>
+                <Link to="/Login" className="navTextRight">
+                  Login
+                </Link>
+                <Link to="/CreateAccount" className="navTextRight">
+                  Create Account
+                </Link>
+              </Flex>
+            </Flex>
+          </Container>
+        </Box>
+      </Box>
     );
-};
-
-export default NavbarComponent;
+  };
+  
+  export default NavbarComponent;
